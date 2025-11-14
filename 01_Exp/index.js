94c20f226fa279243194e3843a3f47b5fb5c416c
Naman Singla <namansingla830@gmail.com>
2025-11-14 13:36:04 +0530

    intall nodemon

--- 01_Exp/index.js
+++ 01_Exp/index.js
@@ -1,11 +1,12 @@
+// 1. Create folder
+// 2. npm init -y and change add type(module)
+// 3. npm i express
+// 4. import express
+// 5. Create instance express
+// 6. Provide port
+// 7. Basic route
+
 import express from 'express'
 const app = express()
-
-// You don't have to worry about this code <ForNow>
-app.get('/', (req, res) => {
-    res.send("Welcome to ExpressJss")
-})
-
-
 
 app.listen(3000, () => console.log("Server Stareted..."));
