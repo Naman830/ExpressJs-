0693d77f6b62f72d681f16829cecfc027ce16549
Naman Singla <namansingla830@gmail.com>
2025-11-14 16:36:58 +0530

    using app.route

--- 03_Postman/index.js
+++ 03_Postman/index.js
@@ -2,20 +2,38 @@ import express from "express";
 const app = express();
 
 // UGLY CODE
-app.get("/student", (req, res) => {
-  res.send("All Students are Here!!!");
-});
+// app.get("/student", (req, res) => {
+//   res.send("All Students are Here!!!");
+// });
 
-app.post("/student", (req, res) => {
-  res.send("Add New Student");
-});
+// app.post("/student", (req, res) => {
+//   res.send("Add New Student");
+// });
 
-app.put("/student", (req, res) => {
-  res.send("Update Student");
-});
+// app.put("/student", (req, res) => {
+//   res.send("Update Student");
+// });
 
-app.delete("/student", (req, res) => {
-  res.send("Delete Student");
-});
+// app.delete("/student", (req, res) => {
+//   res.send("Delete Student");
+// });
+
+// REFACTOR
+// APP.ROUTE()
+
+app
+  .route("/student")
+  .get((req, res) => {
+    res.send("All Students Here!!!");
+  })
+  .post((req, res) => {
+    res.send("Add New Student");
+  })
+  .put((req, res) => {
+    res.send("Update Student");
+  })
+  .delete((req, res) => {
+    res.send("Delete Student");
+  });
 
 app.listen(3000, () => console.log("Server Up..."));
