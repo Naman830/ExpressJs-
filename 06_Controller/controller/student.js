118ff0f79f01fb3a7a947a5b64c2725569f34fea
Naman Singla <namansingla830@gmail.com>
2025-11-15 13:58:50 +0530

    query string multiple query string

--- 06_Controller/controller/student.js
+++ 06_Controller/controller/student.js
@@ -1,19 +1,19 @@
-import express from "express";
+// import express from "express";
 
-const allStudents = (req, res) => {
-  res.send("All Students are Here!!!");
-};
+// const allStudents = (req, res) => {
+//   res.send("All Students are Here!!!");
+// };
 
-const newStudent = (req, res) => {
-  res.send("Add New Student");
-};
+// const newStudent = (req, res) => {
+//   res.send("Add New Student");
+// };
 
-const updateStudent = (req, res) => {
-  res.send("Update Student Id");
-};
+// const updateStudent = (req, res) => {
+//   res.send("Update Student Id");
+// };
 
-const deleteStudent = (req, res) => {
-  res.send("Delete Student");
-};
+// const deleteStudent = (req, res) => {
+//   res.send("Delete Student");
+// };
 
-export { allStudents, newStudent, updateStudent, deleteStudent };
+// export { allStudents, newStudent, updateStudent, deleteStudent };
--- 06_Controller/index.js
+++ 06_Controller/index.js
@@ -1,9 +1,26 @@
-import express from 'express'
-const app = express()
-import students from './routes/student.js'
+import express from "express";
+const app = express();
+// import students from './routes/student.js'
+// app.use('/students', students)
+
+// Query String
+
+// app.get('/product', (req, res) => {
+//     res.send(`response Okay ${req.query.category}`)
+// })
+// answer http://localhost:3000/product?category=anything
 
 
-app.use('/students', students)
+// frontend to backend controlling in query string
+// app.get("/product", (req, res) => {
+//   const { category } = req.query;
+//   res.send(`Product Category: ${category}`);
+// });
 
+// Multiple query string
+app.get("/product", (req, res) => {
+  const { category , id } = req.query;
+  res.send(`Product Category: ${category}, product id: ${id}`);
+});
 
 app.listen(3000, () => console.log("Server Up Baby"));
